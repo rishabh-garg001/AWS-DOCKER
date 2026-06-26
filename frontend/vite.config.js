0afd573adafa5679caaed5015fc214e09339f604
rishabh <rishabhgarg875@gmail.com>
2026-06-26 17:42:39 +0530

    frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: "http://localhost:3000",
+        ws: true,
+        changeOrigin: true,
+      },
+
+      "/health": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      }
+    }
+  }
+})
